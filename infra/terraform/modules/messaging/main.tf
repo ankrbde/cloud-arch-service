4374e635b88f5d9b895e1dc3c9bda04b6e1835a8
ankrbde <ankrbde@gmail.com>
2026-06-15 16:37:52 +0100

    feat(infra): append containers to module main.tf fpr storage and messaging

--- infra/terraform/modules/messaging/main.tf
+++ infra/terraform/modules/messaging/main.tf
@@ -9,3 +9,83 @@ resource "docker_volume" "zookeeper_data" {
 resource "docker_volume" "zookeeper_log" {
   name = "zookeeper_log"
 }
+
+resource "docker_image" "zookeeper" {
+  name = var.zookeeper_image
+}
+
+resource "docker_image" "kafka" {
+  name = var.kafka_image
+}
+
+resource "docker_container" "zookeeper" {
+  name  = "zookeeper"
+  image = docker_image.zookeeper.image_id
+
+  env = [
+    "ZOOKEEPER_CLIENT_PORT=2181",
+    "ZOOKEEPER_TICK_TIME=2000",
+    "ZOOKEEPER_SYNC_LIMIT=2"
+  ]
+
+  ports {
+    internal = 2181
+    external = 2181
+  }
+
+  volumes {
+    volume_name    = docker_volume.zookeeper_data.name
+    container_path = "/var/lib/zookeeper/data"
+  }
+
+  volumes {
+    volume_name    = docker_volume.zookeeper_log.name
+    container_path = "/var/lib/zookeeper/log"
+  }
+
+  networks_advanced {
+    name = var.network_id
+  }
+
+  restart = "unless-stopped"
+}
+
+resource "docker_container" "kafka" {
+  name  = "kafka"
+  image = docker_image.kafka.image_id
+
+  env = [
+    "KAFKA_BROKER_ID=1",
+    "KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181",
+    "KAFKA_LISTENER_SECURITY_PROTOCOL_MAP=PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT",
+    "KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://kafka:9092,PLAINTEXT_HOST://localhost:29092",
+    "KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1",
+    "KAFKA_TRANSACTION_STATE_LOG_MIN_ISR=1",
+    "KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR=1",
+    "KAFKA_GROUP_INITIAL_REBALANCE_DELAY_MS=0",
+    "KAFKA_AUTO_CREATE_TOPICS_ENABLE=true"
+  ]
+
+  ports {
+    internal = 9092
+    external = 9092
+  }
+
+  ports {
+    internal = 29092
+    external = 29092
+  }
+
+  volumes {
+    volume_name    = docker_volume.kafka_data.name
+    container_path = "/var/lib/kafka/data"
+  }
+
+  networks_advanced {
+    name = var.network_id
+  }
+
+  depends_on = [docker_container.zookeeper]
+
+  restart = "unless-stopped"
+}
--- infra/terraform/modules/storage/main.tf
+++ infra/terraform/modules/storage/main.tf
@@ -14,3 +14,106 @@ resource "docker_volume" "minio_data" {
 resource "docker_volume" "redis_data" {
   name = "redis_data"
 }
+
+resource "docker_image" "postgres" {
+  name = var.postgres_image
+}
+
+resource "docker_image" "minio" {
+  name = var.minio_image
+}
+
+resource "docker_image" "redis" {
+  name = var.redis_image
+}
+
+resource "docker_container" "postgres" {
+  name  = "postgres"
+  image = docker_image.postgres.image_id
+
+  env = [
+    "POSTGRES_DB=cloudarch",
+    "POSTGRES_USER=cloudarch",
+    "POSTGRES_PASSWORD=${var.postgres_password}"
+  ]
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+
+  volumes {
+    volume_name    = docker_volume.postgres_data.name
+    container_path = "/var/lib/postgresql/data"
+  }
+
+  networks_advanced {
+    name = docker_network.cloud_arch_net.id
+  }
+
+  healthcheck {
+    test         = ["CMD-SHELL", "pg_isready -U cloudarch"]
+    interval     = "10s"
+    timeout      = "5s"
+    retries      = 5
+    start_period = "10s"
+  }
+
+  restart = "unless-stopped"
+}
+
+resource "docker_container" "minio" {
+  name  = "minio"
+  image = docker_image.minio.image_id
+
+  env = [
+    "MINIO_ROOT_USER=${var.minio_root_user}",
+    "MINIO_ROOT_PASSWORD=${var.minio_root_password}"
+  ]
+
+  command = ["server", "/data", "--console-address", ":9001"]
+
+  ports {
+    internal = 9000
+    external = 9000
+  }
+
+  ports {
+    internal = 9001
+    external = 9001
+  }
+
+  volumes {
+    volume_name    = docker_volume.minio_data.name
+    container_path = "/data"
+  }
+
+  networks_advanced {
+    name = docker_network.cloud_arch_net.id
+  }
+
+  restart = "unless-stopped"
+}
+
+resource "docker_container" "redis" {
+  name  = "redis"
+  image = docker_image.redis.image_id
+
+  command = ["redis-server", "--appendonly", "yes"]
+
+  ports {
+    internal = 6379
+    external = 6379
+  }
+
+  volumes {
+    volume_name    = docker_volume.redis_data.name
+    container_path = "/data"
+  }
+
+  networks_advanced {
+    name = docker_network.cloud_arch_net.id
+  }
+
+  restart = "unless-stopped"
+}
